a676d421e02bf183c2c88c5583057c249c6270e7
kwan <jarupong.wiboonchart@krungsri.com>
2021-01-05 22:51:09 +0700

    Test

--- src/main/java/com/chariotsolutions/example/springboot/datasource/IAMAuthDataSource.java
+++ src/main/java/com/chariotsolutions/example/springboot/datasource/IAMAuthDataSource.java
@@ -2,6 +2,8 @@ package com.chariotsolutions.example.springboot.datasource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 import org.postgresql.ds.PGSimpleDataSource;
 import org.slf4j.Logger;
@@ -12,9 +14,12 @@ import org.slf4j.LoggerFactory;
 //import com.amazonaws.services.rds.auth.GetIamAuthTokenRequest;
 //import com.amazonaws.services.rds.auth.RdsIamAuthTokenGenerator;
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.auth.signer.Aws4Signer;
+import software.amazon.awssdk.auth.signer.params.Aws4PresignerParams;
+import software.amazon.awssdk.http.SdkHttpFullRequest;
+import software.amazon.awssdk.http.SdkHttpMethod;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.rds.RdsUtilities;
-import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
+import software.amazon.awssdk.utils.StringUtils;
 
 
 /**
@@ -38,20 +43,42 @@ extends PGSimpleDataSource
         setProperty("ssl", "true");
         setProperty("sslmode", "require");
 
-
         WebIdentityTokenFileCredentialsProvider webIdentityTokenFileCredentialsProvider = WebIdentityTokenFileCredentialsProvider.create();
-        RdsUtilities rdsUtilities = RdsUtilities.builder()
-                .credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
-                .region(Region.AP_SOUTHEAST_1)
-                .build();
-
-        GenerateAuthenticationTokenRequest buiAuthenticationTokenRequestld = GenerateAuthenticationTokenRequest.builder()
-                .hostname(getServerName())
+        SdkHttpFullRequest httpRequest = SdkHttpFullRequest.builder()
+                .method(SdkHttpMethod.GET)
+                .protocol("https")
+                .host(getServerName())
                 .port(getPortNumber())
-                .username(user)
+                .encodedPath("/")
+                .putRawQueryParameter("DBUser", user)
+                .putRawQueryParameter("Action", "connect")
+                .build();
+        Instant expirationTime = Instant.now().plus(15, ChronoUnit.MINUTES);
+        Aws4PresignerParams preSignRequest = Aws4PresignerParams.builder()
+                .expirationTime(expirationTime)
+                .awsCredentials(webIdentityTokenFileCredentialsProvider.resolveCredentials())
+                .signingName("rds-db")
+                .signingRegion(Region.AP_SOUTHEAST_1)
                 .build();
 
-        String authToken = rdsUtilities.generateAuthenticationToken(buiAuthenticationTokenRequestld);
+        SdkHttpFullRequest fullRequest = Aws4Signer.create().presign(httpRequest, preSignRequest);
+        String signedUrl = fullRequest.getUri().toString();
+        String authToken = StringUtils.replacePrefixIgnoreCase(signedUrl, "https://", "");
+
+
+//
+//        RdsUtilities rdsUtilities = RdsUtilities.builder()
+//                .credentialsProvider(WebIdentityTokenFileCredentialsProvider.create())
+//                .region(Region.AP_SOUTHEAST_1)
+//                .build();
+//
+//        GenerateAuthenticationTokenRequest buiAuthenticationTokenRequestld = GenerateAuthenticationTokenRequest.builder()
+//                .hostname(getServerName())
+//                .port(getPortNumber())
+//                .username(user)
+//                .build();
+//
+//        String authToken = rdsUtilities.generateAuthenticationToken(buiAuthenticationTokenRequestld);
 
 //        DefaultAWSCredentialsProviderChain credentialsProviderChain = new DefaultAWSCredentialsProviderChain();
 //        String awsAccessKey = credentialsProviderChain.getCredentials().getAWSAccessKeyId();
